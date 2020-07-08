ac7bb57beaa89557e9efb314eff184f2033b33da
Leonardo Vitale <leonardo.vitale@outlook.com>
2020-07-08 00:17:16 -0300

    feat: update weather item

--- src/components/WeatherItem/WeatherItem.tsx
+++ src/components/WeatherItem/WeatherItem.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, ForecastTemp, DayOfWeek } from './styles';
+import Icon from '../Icon';
+import TempLabel from '../TempLabel';
+import { Size } from '../TempLabel/styles';
+import format from 'date-fns/format';
 
-const WeatherItem = () => {
-  return <Container>WeatherItem</Container>;
+type WeatherItemProps = {
+  item: any;
+};
+
+const WeatherItem: React.FC<WeatherItemProps> = ({ item }) => {
+  const formatDate = () => {
+    return format(new Date(item.dt * 1000), 'EEEE');
+  };
+
+  return (
+    <Container>
+      <DayOfWeek>{formatDate()}</DayOfWeek>
+      <ForecastTemp>
+        <TempLabel degree={item.temp.day} size={Size.normal} />
+        <Icon name={item.weather[0].id} size='60' />
+      </ForecastTemp>
+    </Container>
+  );
 };
 
 export default WeatherItem;
--- src/components/WeatherItem/styles.tsx
+++ src/components/WeatherItem/styles.tsx
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 340px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const DayOfWeek = styled.div`
+  display: block;
+  font-size: 20px;
+`;
+
+export const ForecastTemp = styled.div`
+  display: flex;
+  align-items: center;
+  align-content: flex-end;
+`;

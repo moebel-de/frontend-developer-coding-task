f49c78e8753f2e6dace34078155559a69ad2764d
Leonardo Vitale <leonardo.vitale@outlook.com>
2020-07-08 00:18:00 -0300

    feat: update weather list

--- src/components/WeatherList/WeatherList.tsx
+++ src/components/WeatherList/WeatherList.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Today } from './styles';
+import WeatherItem from '../WeatherItem';
+import Icon from '../Icon';
+import TempLabel from '../TempLabel';
+import { Size } from '../TempLabel/styles';
 
-const WeatherList = () => {
-  return <Container>WeatherList</Container>;
+interface WeatherListProps {
+  list: any;
+}
+
+type WeatherItemProps = {
+  item: any;
+  index: number;
+};
+
+const WeatherList: React.FC<WeatherListProps> = ({ list }) => {
+  console.log(list);
+
+  return (
+    <Container>
+      {list.map((item: any, index: number) =>
+        index === 0 ? (
+          <Today key={item.dt}>
+            <Icon name={item.weather[0].id} size='60' />
+            <TempLabel degree={item.temp.day} size={Size.big} />
+          </Today>
+        ) : (
+          <WeatherItem key={item.dt} item={item} />
+        )
+      )}
+    </Container>
+  );
 };
 
 export default WeatherList;
--- src/components/WeatherList/styles.tsx
+++ src/components/WeatherList/styles.tsx
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 30px;
+`;
+
+export const Today = styled.div`
+  display: flex;
+  align-items: center;
+`;

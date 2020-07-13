d516a5965d268f1ca8e8a1f460d17ccb70c9c467
Leonardo Vitale <leonardo.vitale@outlook.com>
2020-07-13 10:36:05 +0200

    fix: add api token on .env

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,5 @@
-const apiKey: string = '5d50cb77a4d850371ce5a430e31c9b24';
-
 export const fetchWeather = (city: string) => {
   return fetch(
-    `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=5&units=metric&appid=${apiKey}`
+    `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=5&units=metric&appid=${process.env.REACT_APP_WEATHER_APP_API_TOKEN}`
   );
 };

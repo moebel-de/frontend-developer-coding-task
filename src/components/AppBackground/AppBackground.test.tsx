6cd3a18102155990fa837adac2e625bbed6273b1
Nasser Hassani <snhasani@gmail.com>
2020-07-25 00:58:11 +0430

    test(comp): added AppBackground cold type

--- src/components/AppBackground/AppBackground.test.tsx
+++ src/components/AppBackground/AppBackground.test.tsx
@@ -3,8 +3,19 @@ import { render, screen } from "@testing-library/react";
 
 import AppBackground from "./AppBackground";
 
-test("should render the hot background", () => {
-  render(<AppBackground temperature={20}>So Hot</AppBackground>);
+describe("<AppBackground />", () => {
+  test("should render the hot background", () => {
+    render(<AppBackground temperature={20}>So Hot</AppBackground>);
 
-  expect(screen.getByTestId("app-background")).toHaveAttribute("type", "hot");
+    expect(screen.getByTestId("app-background")).toHaveAttribute("type", "hot");
+  });
+
+  test("should render the cold background", () => {
+    render(<AppBackground temperature={10}>So Cold</AppBackground>);
+
+    expect(screen.getByTestId("app-background")).toHaveAttribute(
+      "type",
+      "cold"
+    );
+  });
 });

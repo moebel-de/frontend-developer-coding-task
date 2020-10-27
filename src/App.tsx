90b56571972c9dda491778bf623f6df5bfd2a370
Abror Abdullaev <aabror1994@gmail.com>
2020-10-27 16:08:06 +0100

    Added component file for city input and static texts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
+import { CityInput } from './Components';
 
 function App() {
   return (
     <div>
       <header>
         <img src={logo} className="App-logo" alt="logo" />
+        <strong>whatweather?</strong>
       </header>
+      <main>
+        <CityInput />
+      </main>
     </div>
   );
 }
---
+++ src/Components/CityInput.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function CityInput() {
+  return (
+    <p>Type in your location and we will tell you what weather to expect.</p>
+  );
+}

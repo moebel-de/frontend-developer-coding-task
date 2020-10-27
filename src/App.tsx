36a92399c065e9825e1f5507be6088b6fb1832bf
Abror Abdullaev <aabror1994@gmail.com>
2020-10-27 17:24:54 +0100

    Added initial layout markup and styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,34 @@ import logo from './logo.svg';
 import Fonts from './Fonts/fonts';
 import { CityInput } from './Components';
 
+const AppDiv = styled.div`
+  text-align: left;
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 30px;
+`;
+
 const BlackSpan = styled.span`
   font-family: roboto-black;
 `;
 
 function App() {
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       <Fonts />
-      <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <BlackSpan>whatweather?</BlackSpan>
-      </header>
-      <main>
-        <CityInput />
-      </main>
-    </div>
+
+      <AppDiv>
+        <header>
+          <div>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <BlackSpan>whatweather?</BlackSpan>
+        </header>
+        <main>
+          <CityInput />
+        </main>
+      </AppDiv>
+    </div >
   );
 }
 

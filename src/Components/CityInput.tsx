347d87bb07b23828ad93d94357bd51c61e730561
Abdullaev <Abror.Abdullaev@pin-ag.local>
2020-10-29 13:25:09 +0100

    Added animated right arrow to input field

--- src/Components/CityInput.tsx
+++ src/Components/CityInput.tsx
@@ -1,5 +1,6 @@
-import React, { ChangeEvent } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import 'weather-icons/css/weather-icons.min.css';
 
 const CityInputField = styled.input`
   width: 100%;
@@ -7,9 +8,10 @@ const CityInputField = styled.input`
   border: none;
   font-size: 1.5em;
   text-align: center;
-  padding: 15px 0;
+  padding: 15px 8% 15px 0;
   box-shadow: 0px 2px 10px 3px rgba(0, 0, 0, 0.2);
   transition: box-shadow .5s;
+  box-sizing: border-box;
 
   &:focus {
     outline: none;
@@ -22,11 +24,42 @@ const CityInputLabel = styled.label`
   margin: 30px 10px 20px;
 `;
 
-export function CityInput(props: { city: string; onChange: (e:string) => void }) {
+const CityInputArrow = styled.i`
+  position: absolute;
+  right: 0;
+  height: 100%;
+  width: 8%;
+
+  &:hover {
+    cursor: pointer;
+
+    &:before {
+      left: calc(50% + 5px);
+    }
+  }
+
+  &:before {
+    transition: left .5s;
+    font-size: 3em;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+export function CityInput(props: {city: string; onChange: (e:string) => void}) {
   return (
     <div>
       <CityInputLabel htmlFor="city">Type in your location and we will tell you what weather to expect.</CityInputLabel>
-      <CityInputField type="text" name="city" placeholder="Type your city" value={props.city} onChange={ e => props.onChange(e.target.value) } />
+      <div style={{ position: 'relative' }}>
+        <CityInputField type="text" 
+          name="city" 
+          placeholder="Type your city" 
+          value={props.city} 
+          onChange={(e) => props.onChange(e.target.value)} />
+        <CityInputArrow className="wi wi-right" />
+      </div>
     </div>
   );
 }
